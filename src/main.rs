27fe0a6e7b55cc7a28fc84b4315d57d038d40fcb
Simon Andersson <simon.asker@posteo.net>
2017-03-29 18:04:17 +0200

    Create very simple image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate image;
+
+use std::fs::File;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let mut imgbuf = image::ImageBuffer::new(100, 100);
+
+    {
+        let mut pixel = imgbuf.get_pixel_mut(50, 50);
+
+        *pixel = image::Luma([255 as u8]);
+    }
+
+    let ref mut fout = File::create(&Path::new("output.png")).unwrap();
+
+    let _  = image::ImageLuma8(imgbuf).save(fout, image::PNG);
 }
