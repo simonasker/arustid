b63d93e7994412a3dd6d4e865df57b69758290bf
Simon Andersson <simon.asker@posteo.net>
2017-03-29 18:51:07 +0200

    Swap colors

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,11 @@ use std::fs::File;
 use std::path::Path;
 
 fn main() {
-    let mut imgbuf = image::ImageBuffer::new(100, 100);
+    let base_pixel = image::Rgb([255 as u8, 255 as u8, 255 as u8]);
+    let mut imgbuf = image::ImageBuffer::from_pixel(100, 100, base_pixel);
 
-    {
-        for i in 0..100 {
-            imgbuf.put_pixel(30, i, image::Rgb([255 as u8, 255 as u8, 255 as u8]));
-        }
+    for i in 0..100 {
+        imgbuf.put_pixel(30, i, image::Rgb([0 as u8, 0 as u8, 0 as u8]));
     }
 
     let ref mut fout = File::create(&Path::new("output.png")).unwrap();
